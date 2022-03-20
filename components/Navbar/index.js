29398743fb82acc3687048e44d6557c8fb41945b
dipstech1 <panja.souradeep@gmail.com>
2022-03-20 19:18:52 +0530

    navbar added

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,14 +1,30 @@
-import styles from './navbar.module.css'
+import Link from 'next/link';
+import styles from './navbar.module.css';
+
 const Navbar = () => {
     return (
         <>
-            <ul className={styles.navUl}>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a >Cart</a></li>
-                <li><a >Item</a></li>
-                <li><a >Logout</a></li>
-            </ul>
+            <nav class={`${styles.navbg} navbar navbar-expand-sm`}>
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link href='/dashboard'>
+                                   <a  className={`${styles.navlinkcolor} nav-link`} >Home</a>
+                                </Link>
+                            </li>
+                            <li class="nav-item">
+                                <a className={`${styles.navlinkcolor} nav-link`} >Profile</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
