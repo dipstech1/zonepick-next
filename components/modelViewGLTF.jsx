0ff145e7878f999809f5d18bfc8f228f79e7e0d5
Sudipta Sarkar <sudipta.sarkar4545@gmail.com>
2022-05-27 12:26:14 +0530

    AR implementaion

--- components/modelViewGLTF.jsx
+++ components/modelViewGLTF.jsx
@@ -2,15 +2,17 @@ import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import {Loader, useGLTF} from "@react-three/drei"
+import { ARCanvas,VRCanvas, useHitTest } from "@react-three/xr";
 
 
 const ModelLoader = (props) => {
   const ref = useRef()
   let fbx = useGLTF(props.source)
+  
   return (
       <Suspense>
           <mesh ref={ref} scale={props.scale} material-reflectivity={1}>
-              <primitive object={fbx.scene} dispose={null}/>
+              <primitive object={fbx.scene} dispose={null} />
           </mesh>
       </Suspense>
   )
@@ -18,17 +20,18 @@ const ModelLoader = (props) => {
 
 const ModelViewGTLF = (props) => {
   const ref = useRef();
+  
   return (
-    <div>
-      <Suspense fallback={<Loader/>}>
-        <Canvas style={{minHeight:'300px',minWidth:'300px'}}>
+    <div>      
+      <Suspense fallback={<Loader/>} >
+        <ARCanvas style={{minHeight:'300px',minWidth:'300px'}}>
           <OrbitControls />
           <ambientLight intensity={0.1} />
           <directionalLight intensity={0.1} />
           <ambientLight />
           <ModelLoader scale={props.scale} source={props.source} />
-        </Canvas>
-      </Suspense>
+        </ARCanvas>
+      </Suspense>     
     </div>
   );
 };
